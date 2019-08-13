f50e02019e67d995f8e665a2d8ddf9e35d04494f
Andrzej Blazejowski <ablazej@softsystem.pl>
2019-08-13 19:13:57 +0200

    seting up variables

--- circles/scripts/index.js
+++ circles/scripts/index.js
@@ -16,7 +16,7 @@ requirejs(['physicsjs',
     'physicsjs/renderers/canvas',
     'physicsjs/behaviors/body-collision-detection',
     'physicsjs/behaviors/edge-collision-detection',
-    'physicsjs/behaviors/newtonian',
+    /*'physicsjs/behaviors/newtonian',*/
     'physicsjs/behaviors/attractor',
     'physicsjs/behaviors/constant-acceleration',
     'physicsjs/behaviors/body-impulse-response',
@@ -30,6 +30,7 @@ requirejs(['physicsjs',
                 ,edgeBounce
                 ,renderer
                 ;
+            const colors = ['#87ECFF', '#7BE8DB', '#94FFD5', '#7BE89E', '#87FF8B'];
             // create a renderer
             renderer = Physics.renderer('canvas', {
                 el: 'viewport'
@@ -45,7 +46,7 @@ requirejs(['physicsjs',
             // constrain objects to these bounds
             edgeBounce = Physics.behavior('edge-collision-detection', {
                 aabb: viewportBounds
-                ,restitution: 1
+                ,restitution: 0.8
                 ,cof: 0
             });
 
@@ -60,29 +61,28 @@ requirejs(['physicsjs',
             }, true);
 
             // create some bodies
-            for( let i = 0; i<=40; i++ ){
-                let velocityX =  Math.floor((Math.random() /4 - 0.125 )*10)/10;
-                let velocityY =  Math.floor((Math.random() /4 - 0.125 )*10)/10;
-                console.log( velocityX, velocityY );
+            for( let i = 0; i<=100; i++ ){
+                let velocityX =  Math.floor((Math.random() /4 - 0.125 )*100)/100;
+                let velocityY =  Math.floor((Math.random() /4 - 0.125 )*100)/100;
                 world.add( Physics.body('circle', {
                     x: renderer.width * Math.random()
                     ,y: renderer.height * Math.random()
-                    // ,vx: velocityX
-                    // ,vy: velocityY
-                    // ,mass: 1
+                    ,vx: velocityX
+                    ,vy: velocityY
+                    ,mass: 1
                     ,restitution: 1
                     ,cof: 0
-                    ,radius: Math.floor(Math.random() * (60 - 40 + 1) + 40)
+                    ,radius: Math.floor(Math.random() * (35 - 15 + 1) + 15)
                     ,styles: {
-                        fillStyle: '#6c71c4'
-                        ,angleIndicator: '#3b3e6b'
+                        fillStyle: colors[i%5]
+                        ,angleIndicator: colors[i%5]
                     }
                 }));
             }
 
             // add some fun interaction
             var attractor = Physics.behavior('attractor', {
-                order: 0,
+                order: 0.45,
                 strength: 0.002
             });
             world.on({
@@ -92,18 +92,26 @@ requirejs(['physicsjs',
                     world.add( attractor );
                 }
                 ,'interact:move': function( pos ){
-                    attractor.position( pos );
+                    if( pos.body ){
+                        //pos.body.accelerate();
+                        console.log(1);
+                    }else{
+                        attractor.position( pos );
+                    }
                 }
                 ,'interact:release': function(){
                     world.wakeUpAll();
                     world.remove( attractor );
                 }
+                ,'interact:grab': function( data ){
+                    console.log( data.body );
+                }
             });
 
             // add things to the world
             world.add([
-                Physics.behavior('interactive', { el: renderer.container })
-                ,Physics.behavior('constant-acceleration')
+                Physics.behavior('interactive', { el: renderer.container, minVel: {x: -0.5, y: -0.5}, maxVel: {x: 0.5, y: 0.5} })
+                /*,Physics.behavior('constant-acceleration')*/
                 ,Physics.behavior('body-impulse-response')
                 ,Physics.behavior('sweep-prune')
                 ,Physics.behavior('body-collision-detection', { checkAll: false })
